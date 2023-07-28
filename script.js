5a8532a029e3a2f122467c9fb9744148a4039d89
largefort <106451813+largefort@users.noreply.github.com>
2023-07-28 14:52:16 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,19 @@
 let clickCount = 0;
 let cursorCount = 0;
 let grandmaCount = 0;
-let factoryCount = 0;
-let bakeryCount = 0;
-let mineCount = 0;
 
 let cursorCost = 10;
 let grandmaCost = 50;
-let factoryCost = 200;
-let bakeryCost = 500;
-let mineCost = 1000;
 
 let cookiesPerSecond = 0;
 let prestigePoints = 0;
 
-let achievements = {
-  firstClick: false,
-  cookies1k: false,
-  cookies10k: false,
-  cookies100k: false,
-};
+let cursorImage = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAQAAAAngNWGAAAAr0lEQVR42mJkwAMYgk9SPR1glIoagpPEaIh9DpUdQwWICiRfIUelJRlAfFIE8IlBEyiIyE6FAEzQFEdDC5BMkQkCVkQjJKAJEVQhSiRfXKBEykhSR6ElKQFM0FHz4zgQQXQADPwhA+lyFvLIAAAAAElFTkSuQmCC';
+let grandmaImage = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAQAAAAngNWGAAAAz0lEQVR42mJkwAMYgk9SPR1glIoagi5ph9HscRAweBpUdQwWICiRfIUelJRlAfFIE8IlBEyiIyE6FAEzQFEdDC5BMkQkCVkQjJKAJEVQhSiRfXKBEykhSR6ElKQFM0FHz4zgQQXQADPwhA+lyFvLIAAAAASUVORK5CYII=';
 
 function clickCookie() {
-  clickCount += 1 + cursorCount + grandmaCount * 5 + factoryCount * 20 + bakeryCount * 50 + mineCount * 100;
+  clickCount += 1 + cursorCount;
   updateClickCount();
-  checkAchievements();
 }
 
 function updateClickCount() {
@@ -53,89 +42,17 @@ function buyUpgrade(upgradeId) {
         updateUpgrades();
       }
       break;
-    case 3: // Factory
-      if (clickCount >= factoryCost) {
-        factoryCount++;
-        clickCount -= factoryCost;
-        factoryCost *= 2;
-        cookiesPerSecond += 20;
-        updateClickCount();
-        updateUpgrades();
-      }
-      break;
-    case 4: // Bakery
-      if (clickCount >= bakeryCost) {
-        bakeryCount++;
-        clickCount -= bakeryCost;
-        bakeryCost *= 2;
-        cookiesPerSecond += 50;
-        updateClickCount();
-        updateUpgrades();
-      }
-      break;
-    case 5: // Mine
-      if (clickCount >= mineCost) {
-        mineCount++;
-        clickCount -= mineCost;
-        mineCost *= 2;
-        cookiesPerSecond += 100;
-        updateClickCount();
-        updateUpgrades();
-      }
-      break;
   }
 }
 
 function updateUpgrades() {
   document.getElementById("click-button").textContent = `Click Me! (Cursor: +${cursorCount})`;
   document.getElementById("click-button").style.backgroundColor = cursorCount > 0 ? "#2ecc71" : "#3498db";
-  document.querySelector(".upgrade-item:nth-child(2) span").textContent = `Upgrade 2: Grandma (+${5 * grandmaCount} per click, Cost: ${grandmaCost})`;
-  document.querySelector(".upgrade-item:nth-child(3) span").textContent = `Upgrade 3: Factory (+${20 * factoryCount} per click, Cost: ${factoryCost})`;
-  document.querySelector(".upgrade-item:nth-child(4) span").textContent = `Upgrade 4: Bakery (+${50 * bakeryCount} per click, Cost: ${bakeryCost})`;
-  document.querySelector(".upgrade-item:nth-child(5) span").textContent = `Upgrade 5: Mine (+${100 * mineCount} per click, Cost: ${mineCost})`;
-  updateCPS();
-}
-
-function checkAchievements() {
-  if (!achievements.firstClick && clickCount >= 1) {
-    showAchievement("First Click!", "You clicked the cookie for the first time.");
-    achievements.firstClick = true;
-  }
-
-  if (!achievements.cookies1k && clickCount >= 1000) {
-    showAchievement("1,000 Cookies!", "You've collected 1,000 cookies.");
-    achievements.cookies1k = true;
-  }
-
-  if (!achievements.cookies10k && clickCount >= 10000) {
-    showAchievement("10,000 Cookies!", "You've collected 10,000 cookies.");
-    achievements.cookies10k = true;
-  }
-
-  if (!achievements.cookies100k && clickCount >= 100000) {
-    showAchievement("100,000 Cookies!", "You've collected 100,000 cookies.");
-    achievements.cookies100k = true;
-  }
-}
-
-function showAchievement(title, description) {
-  const achievementBox = document.createElement("div");
-  achievementBox.className = "achievement-box";
-  achievementBox.textContent = title;
-  achievementBox.title = description;
-
-  document.body.appendChild(achievementBox);
-
-  setTimeout(() => {
-    achievementBox.style.opacity = 1;
-  }, 100);
-
-  setTimeout(() => {
-    achievementBox.style.opacity = 0;
-    setTimeout(() => {
-      document.body.removeChild(achievementBox);
-    }, 1000);
-  }, 5000);
+  document.querySelector(".upgrade-item:nth-child(2) img").src = cursorImage;
+  document.querySelector(".upgrade-item:nth-child(2) span").textContent = `Upgrade 1: Cursor (+1 per click, Cost: ${cursorCost})`;
+  document.querySelector(".upgrade-item:nth-child(3) img").src = grandmaImage;
+  document.querySelector(".upgrade-item:nth-child(3) span").textContent = `Upgrade 2: Grandma (+${5 * grandmaCount} per click, Cost: ${grandmaCost})`;
+  // Add more upgrades with virtual images here
 }
 
 function updateCPS() {
@@ -147,28 +64,13 @@ function resetGame() {
   clickCount = 0;
   cursorCount = 0;
   grandmaCount = 0;
-  factoryCount = 0;
-  bakeryCount = 0;
-  mineCount = 0;
   cursorCost = 10;
   grandmaCost = 50;
-  factoryCost = 200;
-  bakeryCost = 500;
-  mineCost = 1000;
   cookiesPerSecond = 0;
   updateClickCount();
   updateUpgrades();
   updateCPS();
   updatePrestigePoints();
-}
-
-function buyPrestigeUpgrade() {
-  if (prestigePoints >= 100) {
-    prestigePoints -= 100;
-    cookiesPerSecond += 500;
-    updateCPS();
-    updatePrestigePoints();
-  }
 }
 
 function updatePrestigePoints() {
